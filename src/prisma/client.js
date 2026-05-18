e785103b18aa8f44597da2f73ad2a4ebefff9b3e
carreira28 <carreira907@gmail.com>
2026-05-18 14:40:51 +0100

    Update scripts to src/server.js; set Prisma URL
    Change npm scripts to point at src/server.js (add main, update start and dev). Update Prisma client to pass accelerateUrl from process.env.DATABASE_URL and extend with withAccelerate() so the Prisma Accelerate extension uses the configured database URL.

--- src/prisma/client.js
+++ src/prisma/client.js
@@ -1,6 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const { withAccelerate } = require("@prisma/extension-accelerate");
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate());
 
 module.exports = prisma;
