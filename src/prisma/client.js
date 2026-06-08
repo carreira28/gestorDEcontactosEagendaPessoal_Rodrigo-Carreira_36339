e814bc21bfd64bcc2b8e9cd74cea7783c1a24cb1
carreira28 <carreira907@gmail.com>
2026-06-08 14:43:20 +0100

    Update client.js

--- src/prisma/client.js
+++ src/prisma/client.js
@@ -1,11 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
+  datasourceUrl: process.env.DATABASE_URL,
 });
 
 module.exports = prisma;
