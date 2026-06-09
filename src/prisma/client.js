2d2a892d79e51251839574c91713d7ac3b933a15
carreira28 <carreira907@gmail.com>
2026-06-09 14:00:45 +0100

    Update client.js

--- src/prisma/client.js
+++ src/prisma/client.js
@@ -8,7 +8,10 @@ if (process.env.NODE_ENV === "production") {
   const adapter = new PrismaPg(process.env.DATABASE_URL);
   prisma = new PrismaClient({ adapter });
 } else {
-  prisma = new PrismaClient().$extends(withAccelerate());
+  const { PrismaClient: PC } = require("@prisma/client");
+  prisma = new PC({
+    accelerateUrl: process.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 }
 
 module.exports = prisma;
