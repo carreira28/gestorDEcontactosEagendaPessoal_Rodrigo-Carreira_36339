28fa2f2a88629bcc9b15041a0bcdbdfc2b41d7f0
carreira28 <carreira907@gmail.com>
2026-06-09 13:45:28 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,10 +6,8 @@ const app = require("./app");
 
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3000;
 
-if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => {
-        console.log(`✅ Servidor a correr em http://localhost:${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+  console.log(`✅ Servidor a correr na porta ${PORT}`);
+});
 
 module.exports = app;
